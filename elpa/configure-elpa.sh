556fbb38ef355f240c1926f22277f82a2e41c3c3
Hans Pabst <hans.pabst@intel.com>
2017-02-06 22:21:43 +0100

    Updated copyright year (2017).

--- elpa/configure-elpa.sh
+++ elpa/configure-elpa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #############################################################################
-# Copyright (c) 2013-2016, Intel Corporation                                #
+# Copyright (c) 2013-2017, Intel Corporation                                #
 # All rights reserved.                                                      #
 #                                                                           #
 # Redistribution and use in source and binary forms, with or without        #
