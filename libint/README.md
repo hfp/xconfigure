97febf650fbbd44e9f9ff49cc321f25d453f2281
Hans Pabst <hans.pabst@intel.com>
2017-02-14 10:51:37 -0800

    Added configure wrapper scripts for LIBINT along with basic instructions (README.md).

--- libint/README.md
+++ libint/README.md
@@ -1,7 +1,45 @@
 ## LIBINT
 
 ### Build Instructions
-TBD
+[Download](https://github.com/evaleev/libint/archive/release-1-1-6.tar.gz) and unpack LIBINT, and make the configure wrapper scripts available in LIBINT's root folder.
+
+```
+wget https://github.com/evaleev/libint/archive/release-1-1-6.tar.gz
+tar xvf release-1-1-6.tar.gz
+cd libint-release-1-1-6
+wget https://github.com/hfp/xconfigure/raw/master/configure-get.sh
+chmod +x configure-get.sh
+./configure-get.sh libint
+```
+
+Please make the Intel Compiler available on the command line. This actually depends on the environment. For instance, many HPC centers rely on `module load`.
+
+```
+source /opt/intel/compilers_and_libraries_2017.0.098/linux/bin/compilervars.sh intel64
+```
+
+For example, to configure and make for an Intel Xeon E5v4 processor (formerly codenamed "Broadwell"):
+
+```
+./configure-libint-hsw.sh
+make -j ; make install
+```
+
+The version 1.x line of LIBINT does not support to cross-compile for an architecture (a future version of the wrapper scripts may patch this ability into LIBINT 1.x). Therefore, one might rely on the [Intel Software Development Emulator](https://software.intel.com/en-us/articles/intel-software-development-emulator) (Intel SDE) to compile LIBINT for targets, which cannot execute on the compile-host.
+
+```
+/software/intel/sde/sde -knl -- make
+```
+
+To speed-up compilation, "make" might be carried out in phases: after "printing the code" (c-files), the make execution continues with building the object-file where no SDE needed. The latter phase can be sped up by interrupting "make", and executing it without SDE. The root cause of the entire problem is that the driver printing the c-code is (needlessly) compiled using the architecture-flags that are not supported on the host.
+
+Further, for different targets (instruction set extensions) or different versions of the Intel Compiler, the configure scripts support an additional argument ("default" is the default tagname):
+
+```
+./configure-libint-hsw.sh tagname
+```
+
+As shown above, an arbitrary "tagname" can be given (without editing the script). This might be used to build multiple variants of the LIBINT library.
 
 ### References
 TBD
---
+++ libint/configure-libint-skx.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#############################################################################
+# Copyright (c) 2017, Intel Corporation                                     #
+# All rights reserved.                                                      #
+#                                                                           #
+# Redistribution and use in source and binary forms, with or without        #
+# modification, are permitted provided that the following conditions        #
+# are met:                                                                  #
+# 1. Redistributions of source code must retain the above copyright         #
+#    notice, this list of conditions and the following disclaimer.          #
+# 2. Redistributions in binary form must reproduce the above copyright      #
+#    notice, this list of conditions and the following disclaimer in the    #
+#    documentation and/or other materials provided with the distribution.   #
+# 3. Neither the name of the copyright holder nor the names of its          #
+#    contributors may be used to endorse or promote products derived        #
+#    from this software without specific prior written permission.          #
+#                                                                           #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT         #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR     #
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      #
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,    #
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED  #
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR    #
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    #
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      #
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        #
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              #
+#############################################################################
+# Hans Pabst (Intel Corp.)
+#############################################################################
+
+if [ "" = "$1" ]; then PRFX=default-; else PRFX=$1-; shift; fi
+HERE=$(cd $(dirname $0); pwd -P)
+DEST=${HERE}/../libint/${PRFX}
+
+if [ "${HERE}" = "${DEST}" ]; then
+  echo "Warning: LIBINT source directory equals installation folder!"
+  read -p "Are you sure? Y/N" -n 1 -r
+  if [ ! $REPLY =~ ^[Yy]$ ]; then
+    exit 1
+  fi
+fi
+
+FPFLAGS=""
+CONFOPTS=""
+TARGET="-xCORE-AVX512"
+
+export FLAGS="-O2 ${TARGET} -ipo-separate"
+export LDFLAGS=""
+export CFLAGS="${FLAGS} ${FPFLAGS}"
+export CXXFLAGS="${CFLAGS}"
+export FCFLAGS="${FLAGS} -align array64byte"
+export LIBS=""
+
+export AR="xiar"
+export FC="ifort"
+export CC="icc"
+export CXX="icpc"
+
+./configure --prefix=${DEST} ${CONFOPTS} \
+  --with-cc-optflags="-O2 -xCORE-AVX2" \
+  --with-cxx-optflags="-O2 -xCORE-AVX2" \
+  --with-libderiv-max-am1=4 \
+  --with-libint-max-am=5 \
+  $*
+
