27c73b47d2b6ad21299b92960d2cd40d76923f8b
Hans Pabst <hans.pabst@intel.com>
2017-10-30 16:21:06 +0100

    Ensure to rely on Bash.

--- config/qe/configure-qe-hsw-omp.sh
+++ config/qe/configure-qe-hsw-omp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #############################################################################
 # Copyright (c) 2014-2017, Intel Corporation                                #
 # All rights reserved.                                                      #
