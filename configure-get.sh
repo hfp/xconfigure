94939f0c005181e32f5f208ae5b9dfaa2f20f3cf
Hans Pabst <hans.pabst@intel.com>
2018-10-16 14:49:08 +0200

    Improved checking prerequisites. Allow .filelist with optional destination subdirectory for each file, and absolute download locations/URLs.

--- configure-get.sh
+++ configure-get.sh
@@ -30,12 +30,16 @@
 # Hans Pabst (Intel Corp.)
 #############################################################################
 
-BASENAME=$(which basename 2> /dev/null)
-CHMOD=$(which chmod 2> /dev/null)
-WGET=$(which wget 2> /dev/null)
-CAT=$(which cat 2> /dev/null)
-LS=$(which ls 2> /dev/null)
-RM=$(which rm 2> /dev/null)
+BASENAME=$(which basename 2>/dev/null)
+CHMOD=$(which chmod 2>/dev/null)
+WGET=$(which wget 2>/dev/null)
+ECHO=$(which echo 2>/dev/null)
+CAT=$(which cat 2>/dev/null)
+CUT=$(which cut 2>/dev/null)
+TR=$(which tr 2>/dev/null)
+LS=$(which ls 2>/dev/null)
+RM=$(which rm 2>/dev/null)
+MV=$(which mv 2>/dev/null)
 
 BASEURL=https://github.com/hfp/xconfigure/raw/master/config
 ERROR_NOTFOUND=8
@@ -43,18 +47,22 @@ APPLICATION=$1
 ARCHS=$2
 KINDS=$3
 
-if [ "" = "${CHMOD}" ] || [ "" = "${WGET}" ] || [ "" = "${CAT}" ] || [ "" = "${RM}" ]; then
-  echo "Error: prerequisites not found!"
+if [ "" = "${CHMOD}" ] || [ "" = "${WGET}" ] || [ "" = "${ECHO}" ] || \
+   [ "" = "${CAT}" ] || [ "" = "${CUT}" ] || [ "" = "${TR}" ] || \
+   [ "" = "${LS}" ] || [ "" = "${RM}" ] || [ "" = "${MV}" ] || \
+   [ "" = "${BASENAME}" ];
+then
+  ${ECHO} "Error: prerequisites not found!"
   exit 1
 fi
 WGET="${WGET} --no-check-certificate"
 
 if [ "" = "${APPLICATION}" ]; then
-  echo "Please use: $0 <application-name>"
+  ${ECHO} "Please use: $0 <application-name>"
   exit 1
 fi
-if [ "0" != $(${WGET} -S --spider ${BASEURL}/${APPLICATION}/README.md 2> /dev/null; echo $?) ]; then
-  echo "Error: cannot find a recipe for application \"${APPLICATION}\"!"
+if [ "0" != $(${WGET} -S --spider ${BASEURL}/${APPLICATION}/README.md 2>/dev/null; ${ECHO} $?) ]; then
+  ${ECHO} "Error: cannot find a recipe for application \"${APPLICATION}\"!"
   exit 1
 fi
 
@@ -65,58 +73,66 @@ fi
 if [ "" = "${KINDS}" ]; then
   KINDS="omp gnu gnu-omp"
   for KIND in ${KINDS} ; do
-    if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${KIND}.sh 2>${MSGBUFFER}; echo $?)" ]; then
+    if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${KIND}.sh 2>${MSGBUFFER}; ${ECHO} $?)" ]; then
       ${CAT} ${MSGBUFFER}
     fi
   done
   for ARCH in ${ARCHS} ; do
-    if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}.sh 2>${MSGBUFFER}; echo $?)" ]; then
+    if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}.sh 2>${MSGBUFFER}; ${ECHO} $?)" ]; then
       ${CAT} ${MSGBUFFER}
     fi
     for KIND in ${KINDS} ; do
-      if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}-${KIND}.sh 2>${MSGBUFFER}; echo $?)" ]; then
+      if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}-${KIND}.sh 2>${MSGBUFFER}; ${ECHO} $?)" ]; then
         ${CAT} ${MSGBUFFER}
       fi
     done
   done
-  if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}.sh 2>${MSGBUFFER}; echo $?)" ]; then
+  if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}.sh 2>${MSGBUFFER}; ${ECHO} $?)" ]; then
     ${CAT} ${MSGBUFFER}
   fi
 else
   for ARCH in ${ARCHS} ; do
     for KIND in ${KINDS} ; do
-      if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}-${KIND}.sh 2>${MSGBUFFER}; echo $?)" ]; then
+      if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}-${KIND}.sh 2>${MSGBUFFER}; ${ECHO} $?)" ]; then
         ${CAT} ${MSGBUFFER}
       fi
     done
   done
-  if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}.sh 2>${MSGBUFFER}; echo $?)" ]; then
+  if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}.sh 2>${MSGBUFFER}; ${ECHO} $?)" ]; then
     ${CAT} ${MSGBUFFER}
   fi
 fi
 
 # attempt to get a list of non-default file names, and then download each file
-if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/.filelist 2>${MSGBUFFER}; echo $?)" ]; then
+if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/.filelist 2>${MSGBUFFER}; ${ECHO} $?)" ]; then
   ${CAT} ${MSGBUFFER}
 fi
 if [ -e .filelist ]; then
-  for FILE in $(${CAT} .filelist); do
-    ${WGET} -N ${BASEURL}/${APPLICATION}/${FILE}
+  for LINE in $(${CAT} .filelist | ${TR} -s " "); do
+    FILE=$(${ECHO} "${LINE}" | ${CUT} -d" " -f1)
+    DIR=$(${ECHO} "${LINE}" | ${CUT} -d" " -f2)
+    if [ "${FILE}" =~ "://" ]; then
+      ${WGET} -N ${FILE}
+    else
+      ${WGET} -N ${BASEURL}/${APPLICATION}/${FILE}
+    fi
+    if [ -d ${DIR} ]; then
+      ${MV} $(${BASENAME} ${FILE}) ${DIR}
+    fi
   done
   # cleanup list of file names
   ${RM} .filelist
 fi
 
+# make all scripts executable (beyond application)
+${CHMOD} +x *.sh 2>/dev/null
 # cleanup message buffer
 ${RM} ${MSGBUFFER}
 
-if [ "" != "$(${LS} -1 configure-${APPLICATION}*.sh 2> /dev/null)" ]; then
-  # make all scripts executable (beyond application)
-  ${CHMOD} +x *.sh 2> /dev/null
-else
+if [ "" = "$(${LS} -1 configure-${APPLICATION}*.sh 2>/dev/null)" ]; then
   # display reminder about build recipe
-  echo
-  echo "There is no configuration needed! Please read:"
-  echo "https://xconfigure.readthedocs.io/${APPLICATION}/README/"
+  ${ECHO}
+  ${ECHO} "There is no configuration needed! Please read:"
+  ${ECHO} "https://xconfigure.readthedocs.io/${APPLICATION}/README/"
 fi
 
