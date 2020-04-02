4e06d7c97244097a851d68d0be3c0e8e6d015989
Hans Pabst <hans.pabst@intel.com>
2020-04-02 12:37:54 +0200

    Added script.

---
+++ config/cp2k/inject.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the XCONFIGURE project.                                #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/xconfigure/                     #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.)
+###############################################################################
+SED=$(command -v sed)
+
+if [ "" != "$1" ] && [ "" != "$2" ] && [ "" != "$3" ] && [ "" != "${SED}" ]; then
+  MATCH=$1
+  FLAGS=$2
+  EXE=$3
+  shift 3
+  if [ "" != "$1" ] && [ "" != "$(echo "$@" | ${SED} -n "/${MATCH}/p" 2>/dev/null)" ]; then
+    CMD="${EXE} ${FLAGS} $@"
+  elif [ "" != "$1" ]; then
+    CMD="${EXE} $@"
+  else
+    CMD="${EXE}"
+  fi
+  echo "Executing: \"${CMD}\""
+  exec ${CMD}
+else
+  echo "Error: missing prerequisites!"
+  exit 1
+fi
