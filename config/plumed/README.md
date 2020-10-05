714005cf09ac31ed3ac29938924ed6a073c21e27
Hans Pabst <hans.pabst@intel.com>
2020-10-05 17:43:38 +0200

    Initial support for Plumed/2.

---
+++ config/plumed/README.md
@@ -0,0 +1,30 @@
+# Plumed
+
+To [download](https://www.plumed.org/download), configure, build, and install [Plumed](https://github.com/plumed/plumed2/releases/latest)&#160;2.x (CP2K requires Plumed2), one may proceed as shown below. See also [How to compile CP2K with Plumed](https://www.cp2k.org/howto:install_with_plumed).
+
+```bash
+wget --no-check-certificate https://github.com/plumed/plumed2/archive/v2.6.1.tar.gz
+tar xvf v2.6.1.tar.gz
+cd plumed2-2.6.1
+
+wget --no-check-certificate https://github.com/hfp/xconfigure/raw/master/configure-get.sh
+chmod +x configure-get.sh
+./configure-get.sh plumed
+```
+
+Please make the intended compiler available on the command line. For instance, many HPC centers rely on `module load`.
+
+```bash
+source /opt/intel/compilers_and_libraries_2020.2.254/linux/bin/compilervars.sh intel64
+```
+
+For example, to configure and make for an Intel Xeon Scalable processor ("SKX"):
+
+```bash
+make distclean
+./configure-plumed-skx.sh
+make -j; make install
+```
+
+**NOTE**: Please disregard messages during configuration suggesting `libtoolize --force`.
+
---
+++ config/plumed/configure-plumed-knl.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the XCONFIGURE project.                                #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/xconfigure/                     #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.)
+###############################################################################
+
+if [ "" = "$1" ]; then PRFX=intel-; else PRFX=$1-; shift; fi
+HERE=$(cd $(dirname $0); pwd -P)
+DEST=${HERE}/../plumed/${PRFX}knl
+
+if [ ! -e ${HERE}/configure.ac ] || [ "${HERE}" != "$(pwd -P)" ]; then
+  echo "Error: XCONFIGURE scripts must be located and executed in the application folder!"
+  exit 1
+fi
+
+if [ "${HERE}" = "${DEST}" ]; then
+  echo "Warning: PLUMED source directory equals installation folder!"
+  read -p "Are you sure? Y/N" -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+fi
+
+CONFOPTS=""
+TARGET="-xMIC-AVX512"
+
+# consider more accurate FP-model
+#FPCMODEL = -fp-model precise
+#FPFMODEL = -fp-model source
+
+export FLAGS="-O2 ${TARGET}" #-ipo-separate
+export LDFLAGS=""
+export CFLAGS="${FLAGS} ${FPCMODEL}"
+export CXXFLAGS="${FLAGS} ${FPCMODEL}"
+export FCFLAGS="${FLAGS} ${FPFMODEL} -align array64byte"
+export LIBS=""
+
+export AR="xiar"
+export FC="ifort"
+export CC="icc"
+export CXX="icpc"
+
+CC_VERSION_STRING=$(${CC} --version 2> /dev/null | head -n1 | sed "s/..* \([0-9][0-9]*\.[0-9][0-9]*\.*[0-9]*\)[ \S]*.*/\1/")
+CC_VERSION_MAJOR=$(echo "${CC_VERSION_STRING}" | cut -d"." -f1)
+CC_VERSION_MINOR=$(echo "${CC_VERSION_STRING}" | cut -d"." -f2)
+CC_VERSION_PATCH=$(echo "${CC_VERSION_STRING}" | cut -d"." -f3)
+CC_VERSION_COMPONENTS=$(echo "${CC_VERSION_MAJOR} ${CC_VERSION_MINOR} ${CC_VERSION_PATCH}" | wc -w)
+if [ "3" = "${CC_VERSION_COMPONENTS}" ]; then
+  CC_VERSION=$((CC_VERSION_MAJOR * 10000 + CC_VERSION_MINOR * 100 + CC_VERSION_PATCH))
+elif [ "2" = "${CC_VERSION_COMPONENTS}" ]; then
+  CC_VERSION=$((CC_VERSION_MAJOR * 10000 + CC_VERSION_MINOR * 100))
+  CC_VERSION_PATCH=0
+else
+  CC_VERSION_STRING=""
+  CC_VERSION=0
+fi
+
+if [ "0" != "$((180000<=CC_VERSION && 180001>CC_VERSION))" ] || \
+   [ "0" != "$((170006>CC_VERSION && 0!=CC_VERSION))" ]; \
+then
+  export CC="${CC} -D_Float128=__float128"
+fi
+
+libtoolize
+aclocal
+autoheader
+automake -a
+autoconf
+
+./configure \
+  --prefix=${DEST} ${CONFOPTS} \
+  --host=x86_64-unknown-linux-gnu \
+  $*
+
