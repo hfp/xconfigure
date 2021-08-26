6df1ae85ed9a072d0b6247155d7ce5cc492e9eed
Hans Pabst <hans.pabst@intel.com>
2021-08-26 16:17:20 +0200

    Added support for libmed. Regenerated documentation.

---
+++ config/libmed/README.md
@@ -0,0 +1,36 @@
+# Med File Library
+
+To [download](https://salome-platform.org/downloads/), configure, build, and install libmed, one may proceed as shown below.
+
+```bash
+wget --no-check-certificate https://files.salome-platform.org/Salome/other/med-4.1.0.tar.gz
+tar xvf med-4.1.0.tar.gz
+cd med-4.1.0
+
+wget --no-check-certificate https://github.com/hfp/xconfigure/raw/master/configure-get.sh
+chmod +x configure-get.sh
+./configure-get.sh libmed
+```
+
+Please make the intended compiler available on the command line. For instance, many HPC centers rely on `module load`.
+
+```bash
+source /opt/intel/compilers_and_libraries_2020.4.304/linux/bin/compilervars.sh intel64
+```
+
+**Note**: Please make the "hdf5-tools" command available, or pay attention to the console output after configuring libmed. In general, an HDF5 development package is necessary to pass the default configuration as implemented by XCONFIGURE. One can adjust the configure wrapper script for custom-built HDF5 by pointing to an (non-default) installation location.
+
+For example, to configure and make for an Intel Xeon Scalable processor ("SKX"):
+
+```bash
+make distclean
+./configure-med-skx.sh
+make -j; make install
+```
+
+## References
+
+[https://salome-platform.org/downloads/](https://salome-platform.org/downloads/)  
+[http://wiki.opentelemac.org/doku.php?id=installation_linux_med](http://wiki.opentelemac.org/doku.php?id=installation_linux_med)  
+[http://opentelemac.org/](http://opentelemac.org/)
+
---
+++ config/libmed/configure-libmed-gnu.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the XCONFIGURE project.                                #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/xconfigure/                     #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.)
+###############################################################################
+
+if [ "" = "$1" ]; then PRFX=gnu; else PRFX=$1; shift; fi
+HERE=$(cd $(dirname $0); pwd -P)
+DEST=${HERE}/../libmed/${PRFX}
+
+if [ ! -e ${HERE}/configure.ac ] || [ "${HERE}" != "$(pwd -P)" ]; then
+  echo "Error: XCONFIGURE scripts must be located and executed in the application folder!"
+  exit 1
+fi
+
+if [ "${HERE}" = "${DEST}" ]; then
+  echo "Warning: LIBMED source directory equals installation folder!"
+  read -p "Are you sure? Y/N" -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+fi
+
+CONFOPTS=""
+TARGET="-march=native"
+
+export FLAGS="-O3 ${TARGET}"
+export LDFLAGS=""
+export CFLAGS="${FLAGS}"
+export CXXFLAGS="${FLAGS}"
+export FCFLAGS="${FLAGS}"
+
+export AR="gcc-ar"
+export FC="mpif90"
+export CC="mpicc"
+export CXX="mpicxx"
+export F77=${FC}
+export F90=${FC}
+
+export MPICC=${CC}
+export MPIFC=${FC}
+export MPIF77=${F77}
+export MPICXX=${CXX}
+
+libtoolize
+aclocal
+#autoheader
+#automake -a
+autoconf
+
+./configure \
+  --prefix=${DEST} ${CONFOPTS} \
+  --host=x86_64-unknown-linux-gnu \
+  $*
+
