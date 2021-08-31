9346f3c3e8baa677a8339c7b54863c99e8c734e3
Hans Pabst <hans.pabst@intel.com>
2021-08-31 22:06:47 +0200

    Added support for Vc.

---
+++ config/vc/README.md
@@ -0,0 +1,33 @@
+# Vc: SIMD Vector Classes for C++
+
+To [download](https://github.com/VcDevel/Vc/releases/latest), configure, build, and install Vc, one may proceed as shown below.
+
+```bash
+wget --no-check-certificate https://github.com/VcDevel/Vc/archive/refs/tags/1.4.2.tar.gz
+tar xvf Vc-1.4.2.tar.gz
+cd Vc-1.4.2
+
+wget --no-check-certificate https://github.com/hfp/xconfigure/raw/master/configure-get.sh
+chmod +x configure-get.sh
+./configure-get.sh vc
+```
+
+Please make the intended compiler available on the command line. For instance, many HPC centers rely on `module load`.
+
+```bash
+source /opt/intel/compilers_and_libraries_2020.4.304/linux/bin/compilervars.sh intel64
+```
+
+For example, to configure and make for an Intel Xeon Scalable processor ("SKX"):
+
+```bash
+make distclean
+./configure-vc-skx.sh
+cd build; make -j; make install
+```
+
+## References
+
+[https://github.com/VcDevel/Vc/releases](https://github.com/VcDevel/Vc/releases)  
+[https://github.com/VcDevel/Vc](https://github.com/VcDevel/Vc)
+
---
+++ config/vc/configure-vc-hsw-gnu.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the XCONFIGURE project.                                #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/xconfigure/                     #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.)
+###############################################################################
+
+if [ "" = "$1" ]; then PRFX=gnu-; else PRFX=$1-; shift; fi
+HERE=$(cd $(dirname $0); pwd -P)
+DEST=${HERE}/../vc/${PRFX}hsw
+CMAKE=$(command -v cmake)
+
+if [ "" = "${CMAKE}" ]; then
+  echo "Error: missing CMake!"
+  exit 1
+fi
+
+if [ ! -e ${HERE}/CMakeLists.txt ] || [ "${HERE}" != "$(pwd -P)" ]; then
+  echo "Error: XCONFIGURE scripts must be located and executed in the application folder!"
+  exit 1
+fi
+
+if [ "${HERE}" = "${DEST}" ]; then
+  echo "Warning: Vc source directory equals installation folder!"
+  read -p "Are you sure? Y/N" -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+else
+  rm -f ${HERE}/CMakeCache.txt
+fi
+
+CONFOPTS="-DBUILD_TESTING=OFF"
+TARGET="-march=core-avx2"
+
+export FLAGS="-fPIC ${TARGET}"
+export LDFLAGS=""
+export CFLAGS="${FLAGS}"
+export CXXFLAGS="${FLAGS}"
+export FCFLAGS="${FLAGS}"
+export F77FLAGS=${FCFLAGS}
+export F90FLAGS=${FCFLAGS}
+export FFLAGS=${FCFLAGS}
+
+export AR="gcc-ar"
+export FC="gfortran"
+export CC="gcc"
+export CXX="g++"
+export F77=${FC}
+export F90=${FC}
+
+rm -rf ${HERE}/build
+mkdir -p ${HERE}/build && cd ${HERE}/build
+
+cmake -DCMAKE_INSTALL_PREFIX=${DEST} ${CONFOPTS} $* ${HERE}
+echo "Remember to \"cd build\" before \"make; make install\""
