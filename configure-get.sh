a18fe6dc94bc9368a64a33d37febe4f172f45c40
Hans Pabst <hans.pabst@intel.com>
2022-01-17 12:38:10 +0100

    Fixed issues pointed out by Shellcheck.

--- configure-get.sh
+++ configure-get.sh
@@ -52,7 +52,7 @@ fi
 
 echo "Be patient, it can take up to 30 seconds before progress is shown..."
 echo
-if [ "$(${WGET} -q -S --spider ${BASEURL}/${APPLICATION}/README.md 2>/dev/null | ${SED} -n '/200 OK/p')" ]; then
+if [ "$(${WGET} -q -S --spider "${BASEURL}/${APPLICATION}/README.md" 2>/dev/null | ${SED} -n '/200 OK/p')" ]; then
   echo "Error: cannot find a recipe for application \"${APPLICATION}\"!"
   exit 1
 fi
@@ -64,53 +64,53 @@ fi
 if [ ! "${KINDS}" ]; then
   KINDS="omp gnu gnu-omp"
   for KIND in ${KINDS}; do
-    if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${KIND}.sh 2>${MSGBUFFER}; echo $?)" ]; then
-      ${SED} "" ${MSGBUFFER}
+    if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N "${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${KIND}.sh" 2>"${MSGBUFFER}"; echo "$?")" ]; then
+      ${SED} "" "${MSGBUFFER}"
     fi
   done
   for ARCH in ${ARCHS}; do
-    if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}.sh 2>${MSGBUFFER}; echo $?)" ]; then
-      ${SED} "" ${MSGBUFFER}
+    if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N "${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}.sh" 2>"${MSGBUFFER}"; echo "$?")" ]; then
+      ${SED} "" "${MSGBUFFER}"
     fi
     for KIND in ${KINDS}; do
-      if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}-${KIND}.sh 2>${MSGBUFFER}; echo $?)" ]; then
-        ${SED} "" ${MSGBUFFER}
+      if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N "${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}-${KIND}.sh" 2>"${MSGBUFFER}"; echo "$?")" ]; then
+        ${SED} "" "${MSGBUFFER}"
       fi
     done
   done
-  if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}.sh 2>${MSGBUFFER}; echo $?)" ]; then
-    ${SED} "" ${MSGBUFFER}
+  if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N "${BASEURL}/${APPLICATION}/configure-${APPLICATION}.sh" 2>"${MSGBUFFER}"; echo "$?")" ]; then
+    ${SED} "" "${MSGBUFFER}"
   fi
 else
   for ARCH in ${ARCHS}; do
     for KIND in ${KINDS}; do
-      if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}-${KIND}.sh 2>${MSGBUFFER}; echo $?)" ]; then
-        ${SED} "" ${MSGBUFFER}
+      if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N "${BASEURL}/${APPLICATION}/configure-${APPLICATION}-${ARCH}-${KIND}.sh" 2>"${MSGBUFFER}"; echo "$?")" ]; then
+        ${SED} "" "${MSGBUFFER}"
       fi
     done
   done
-  if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/configure-${APPLICATION}.sh 2>${MSGBUFFER}; echo $?)" ]; then
-    ${SED} "" ${MSGBUFFER}
+  if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N "${BASEURL}/${APPLICATION}/configure-${APPLICATION}.sh" 2>"${MSGBUFFER}"; echo "$?")" ]; then
+    ${SED} "" "${MSGBUFFER}"
   fi
 fi
 
 # attempt to get a list of non-default file names, and then download each file
-if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N ${BASEURL}/${APPLICATION}/.filelist 2>${MSGBUFFER}; echo $?)" ]; then
-  ${SED} "" ${MSGBUFFER}
+if [ "${ERROR_NOTFOUND}" != "$(${WGET} -N "${BASEURL}/${APPLICATION}/.filelist" 2>"${MSGBUFFER}"; echo "$?")" ]; then
+  ${SED} "" "${MSGBUFFER}"
 fi
 if [ -e .filelist ]; then
   ${TR} -s " " <.filelist | \
-  while read LINE; do
+  while read -r LINE; do
     FILE=$(echo "${LINE}" | ${CUT} -d" " -f1)
     DIR=$(echo "${LINE}" | ${CUT} -d" " -f2)
     if [ "" != "${LINE}" ]; then # skip empty lines
       if [[ "${FILE}" =~ "://" ]]; then
-        ${WGET} -N ${FILE}
+        ${WGET} -N "${FILE}"
       else
-        ${WGET} -N ${BASEURL}/${APPLICATION}/${FILE}
+        ${WGET} -N "${BASEURL}/${APPLICATION}/${FILE}"
       fi
-      if [ "${FILE}" != "${DIR}" ] && [ -d ${DIR} ]; then
-        ${MV} $(${BASENAME} ${FILE}) ${DIR}
+      if [ "${FILE}" != "${DIR}" ] && [ -d "${DIR}" ]; then
+        ${MV} "$(${BASENAME} "${FILE}")" "${DIR}"
       fi
     fi
   done
@@ -119,11 +119,11 @@ if [ -e .filelist ]; then
 fi
 
 # make all scripts executable (beyond application)
-${CHMOD} +x *.sh 2>/dev/null
+${CHMOD} +x ./*.sh 2>/dev/null
 # cleanup message buffer
-${RM} ${MSGBUFFER}
+${RM} "${MSGBUFFER}"
 
-if [ ! "$(${LS} -1 configure-${APPLICATION}*.sh 2>/dev/null)" ]; then
+if [ ! "$(${LS} -1 "configure-${APPLICATION}"*.sh 2>/dev/null)" ]; then
   # display reminder about build recipe
   echo
   echo "There is no configuration needed! Please read:"
