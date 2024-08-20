c6014f967eb824a3a986ba4832fda1578c9339a5
Hans Pabst <hans.pabst@intel.com>
2024-08-20 11:21:02 +0200

    Corrected sort order.

--- config/cp2k/run.sh
+++ config/cp2k/run.sh
@@ -216,8 +216,8 @@ ldd "${EXE}"
 echo
 
 # print environment
-env | sort | grep "^LD_PRELOAD\|^GLIBC_\|^LIBXSMM_\|^CUDA_\|^DBCSR_\|^ACC_\|^DBM_\|^MKL\|^OPENCL_\|^SLURM_"
-env | sort | grep "^I_MPI_\|^PMI_\|^MPICH_\|^PSM3_\|^FI_\|^OMPI_\|^UCX_\|^OMP_\|^KMP_\|^ZEX_\|^IGC_"
+env | grep "^LD_PRELOAD\|^GLIBC_\|^LIBXSMM_\|^CUDA_\|^DBCSR_\|^ACC_\|^DBM_\|^MKL\|^OPENCL_\|^SLURM_\| \
+            ^I_MPI_\|^PMI_\|^MPICH_\|^PSM3_\|^FI_\|^OMPI_\|^UCX_\|^OMP_\|^KMP_\|^ZEX_\|^IGC_" | sort
 echo
 
 # print final command
