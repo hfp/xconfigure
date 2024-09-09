c42859885449ff5e82c972d31ecba10e5c12dfeb
Hans Pabst <hans.pabst@intel.com>
2024-09-09 10:43:21 +0200

    Complement I_MPI_OFFLOAD.

--- config/cp2k/run.sh
+++ config/cp2k/run.sh
@@ -153,6 +153,7 @@ if [ "${I_MPI_ROOT}" ]; then
   if [ ! "${ACC_OPENCL_DEVIDS}" ] && [ ! "${ACC_OPENCL_DEVTYPE}" ] && \
        command -v ldd >/dev/null && ldd "${EXE}" | grep -q libOpenCL;
   then
+    export I_MPI_OFFLOAD_RDMA=${I_MPI_OFFLOAD_RDMA:-0}
     export I_MPI_OFFLOAD=${I_MPI_OFFLOAD:-1}
   else
     export I_MPI_OFFLOAD=0
