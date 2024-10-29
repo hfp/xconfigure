f4803ef941d20e628db38a8751852aaee0270849
Hans Pabst <hans.pabst@intel.com>
2024-10-29 17:11:27 +0100

    Simplified recipe as now supported by CP2K-recipe (ELPA).

--- config/elpa/configure-elpa.sh
+++ config/elpa/configure-elpa.sh
@@ -141,27 +141,3 @@ fi
 if [ -e "${HERE}/Makefile" ]; then
   sed -i "s/all-am:\(.*\) \$(PROGRAMS)/all-am:\1/" Makefile
 fi
-
-if [ -e "${HERE}/config.h" ]; then
-  VERSION=$(grep ' VERSION ' config.h | cut -s -d' ' -f3 | sed -e 's/^\"//' -e 's/\"$//')
-  if [ "" != "${VERSION}" ]; then
-    if [ "0" != "$(grep ' WITH_OPENMP' config.h | cut -s -d' ' -f3 | sed -e 's/^\"//' -e 's/\"$//')" ]; then
-      ELPA=elpa_openmp
-    else
-      ELPA=elpa
-    fi
-    mkdir -p ${DEST}/include/${ELPA}-${VERSION}
-    if [ ! -e ${DEST}/include/elpa ]; then
-      CWD=$(pwd)
-      cd ${DEST}/include
-      ln -s ${ELPA}-${VERSION} elpa
-      cd ${CWD}
-    fi
-    mkdir -p ${DEST}/lib
-    cd ${DEST}/lib
-    ln -fs libelpa_openmp.so libelpa.so
-    ln -fs libelpa_openmp.a libelpa.a
-    ln -fs libelpa.so libelpa_mt.so
-    ln -fs libelpa.a libelpa_mt.a
-  fi
-fi
