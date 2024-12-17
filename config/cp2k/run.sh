2cfddbebc84e03c782cd79d58af5cc71a087afe3
Hans Pabst <hans.pabst@intel.com>
2024-12-17 20:34:49 +0100

    Adjusted run-script.

--- config/cp2k/run.sh
+++ config/cp2k/run.sh
@@ -166,9 +166,6 @@ if [ "${I_MPI_ROOT}" ]; then
     export I_MPI_ADJUST_REDUCE=${I_MPI_ADJUST_REDUCE:-1}
     export I_MPI_ADJUST_BCAST=${I_MPI_ADJUST_BCAST:-1}
   fi
-  if [ ! "${I_MPI_OFI_PROVIDER}" ] && [ "1" != "${NUMNODES}" ]; then
-    export I_MPI_OFI_PROVIDER=psm3
-  fi
   export I_MPI_SHM_HEAP=${I_MPI_SHM_HEAP:-1}
   export I_MPI_DEBUG=${I_MPI_DEBUG:-4}
   #
